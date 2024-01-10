32ff2d05bf08273bbab080dd3c4f7d847c1e3517
7onya <antonia1412299@yahoo.com>
2024-01-10 13:44:53 +0200

    UI: Adding TopBar

---
+++ app/src/main/java/com/crocheteer/crocheteer/ui/components/TopBar.kt
@@ -0,0 +1,45 @@
+package com.crocheteer.crocheteer.ui.components
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "My Yarn Stash",
+                //color = Color.Black,
+//                modifier = Modifier.fillMaxSize(),
+//                textAlign = TextAlign.Center,
+                fontFamily = FontFamily.Cursive,
+                fontWeight = FontWeight.ExtraBold,
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary
+        )
+    )
+}
+
