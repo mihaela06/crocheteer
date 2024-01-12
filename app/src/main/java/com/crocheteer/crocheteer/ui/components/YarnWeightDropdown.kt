3393f8dfe02aa6f44aa6548cc92c6e8a606ecde1
7onya <antonia141299@yahoo.com>
2024-01-12 22:40:58 +0200

    UI: YarnWeightDropdown style improvement

--- app/src/main/java/com/crocheteer/crocheteer/ui/components/YarnWeightDropdown.kt
+++ app/src/main/java/com/crocheteer/crocheteer/ui/components/YarnWeightDropdown.kt
@@ -2,12 +2,22 @@ package com.crocheteer.crocheteer.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,10 +25,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.crocheteer.crocheteer.data.entities.YarnWeight
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun YarnWeightDropdown(
     weight: YarnWeight,
@@ -31,8 +41,24 @@ fun YarnWeightDropdown(
         modifier = modifier
             .wrapContentSize(Alignment.TopStart)
     ) {
-        Text(
-            weight.prettifiedName,
+        OutlinedTextField(
+            value = weight.prettifiedName,
+            onValueChange = {},
+            readOnly = true,
+            shape = RoundedCornerShape(8.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                textColor = MaterialTheme.colorScheme.onSurface,
+                disabledTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                disabledBorderColor = MaterialTheme.colorScheme.outline,
+                disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant
+            ),
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    contentDescription = "Dropdown",
+                    Modifier.clickable { expanded = !expanded }
+                )
+            },
             modifier = modifier
                 .fillMaxWidth()
                 .clickable(onClick = { expanded = true })
@@ -40,7 +66,9 @@ fun YarnWeightDropdown(
         DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false },
-            modifier = modifier.height(200.dp)
+            modifier = modifier
+                .fillMaxWidth(0.9f)
+                .fillMaxHeight(0.4f)
         ) {
             items.forEachIndexed { index, s ->
                 DropdownMenuItem(
@@ -50,17 +78,11 @@ fun YarnWeightDropdown(
                     },
                     text = { Text(text = s.prettifiedName) },
                     modifier = modifier
+                        .fillMaxWidth()
+                        .padding(10.dp)
                 )
+                Divider()
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun YarnWeightPreview() {
-    var weight by remember {
-        mutableStateOf(YarnWeight.Aran)
-    }
-    YarnWeightDropdown(weight = weight, onWeightChange = { weight = it })
 }
